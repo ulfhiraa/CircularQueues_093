114fad3dd3ca75bea9878ed9023e9c5c878555c6
ulfhiraa <studyiraa@gmail.com>
2023-06-23 15:32:08 +0700

    iterasi dari awal array hingga rear

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -88,6 +88,12 @@ public:
 			}
 
 			FRONT_position = 0;
+
+			// iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+			}
+			cout << endl;
 		}
 	}
 };
