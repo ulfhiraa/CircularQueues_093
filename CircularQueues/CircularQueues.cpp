bf018a0f5aadec9cd1d26a38f97cd71778b53908
ulfhiraa <studyiraa@gmail.com>
2023-06-23 15:27:02 +0700

    else, jika posisi front > posisi rear, iterasi dari front hingga akhir array

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -80,5 +80,14 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			// jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+		}
 	}
 };
